cf25bd5ea995556383b3664ee78eddfb2945fde6
Oscar Lillo <osergod999@gmail.com>
2026-01-05 17:01:03 +0100

    Hago el menu principal completo y dos paneles para la tienda y los ajustes

---
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarEscena(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+
+    public void MostrarPanel(GameObject panel)
+    {
+        panel.SetActive(true);
+    }
+    
+    public void OcultarPanel(GameObject panel)
+    {
+        panel.SetActive(false);
+    }
+
+    public void Close()
+    {
+        if (Application.isEditor)
+            UnityEditor.EditorApplication.isPlaying = false;
+        else
+            Application.Quit();
+    }
+
+}
