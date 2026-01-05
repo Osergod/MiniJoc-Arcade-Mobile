3384ac1b63b781e76d0186cb98c1e2e150dee2e2
Oscar Lillo <osergod999@gmail.com>
2026-01-05 17:26:35 +0100

    Preparo la versión 0.7

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -34,10 +34,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void Close()
     {
-        if (Application.isEditor)
-            UnityEditor.EditorApplication.isPlaying = false;
-        else
-            Application.Quit();
+        Application.Quit();
     }
 
 }
